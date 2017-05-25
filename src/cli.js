3cd0f22920f01bd428f2cf3d1632d01996690089
iskaldvind <iskaldvind@gmail.com>
2017-05-25 13:22:51 +0300

    step2 fixes

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import gendiff from './file-comparer';
 
 const main = () => {
   program
-    .version('0.0.59')
+    .version('0.0.60')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig) => {
       console.log(gendiff(firstConfig, secondConfig));
