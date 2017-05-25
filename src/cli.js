f340732e12fb091b76e815f0f14466d7aa303e11
iskaldvind <iskaldvind@gmail.com>
2017-05-25 23:11:44 +0300

    tests unwatched

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import gendiff from './file-comparer';
 
 const main = () => {
   program
-    .version('0.0.64')
+    .version('0.0.65')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig) => {
       console.log(gendiff(firstConfig, secondConfig));
